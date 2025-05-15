ac4b0b41cd47bed16e7ab9fa2098c68bba7e6f38
joaod77 <joaodelgado@ipvc.pt>
2025-05-15 19:02:02 +0100

    Update API Running for Backup Render

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,9 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use(moviesRouter);
 app.use('/api/comments', commentRoutes);
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
